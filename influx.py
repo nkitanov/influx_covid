beb05765c127323d50ba5f3ab7994c5bcc303108
Nikolay Kitanov <nki@melexis.com>
2020-03-15 13:38:11 +0200

    update

--- influx.py
+++ influx.py
@@ -1,31 +1,27 @@
+#!/usr/bin/env python3
+
 from influxdb import InfluxDBClient
 
 client = InfluxDBClient(host='192.168.1.201', port=8086, database='home_assistant')
+avg=client.query('select * from covid_confirmed_average;')
+avg_list = []
 
+# https://influxdb-python.readthedocs.io/en/latest/resultset.html
+for i in avg.get_points():
+    avg_list.append(i)
 
 def diff():
-    avg_list = []
-    avg=client.query('select * from covid_confirmed_average;')
-    for i in avg.get_points():
-        avg_list.append(i)
     fin = avg_list[-1]['last']
     prev = avg_list[-2]['last']
     return float(fin-prev)
 
 def rate():
-    rate = []
-    dq=client.query('select * from covid_confirmed_average;')
-    for i in dq.get_points():
-        rate.append(i)
-    first = rate[-1]['last']
-    second = rate[-2]['last']
-    third = rate[-3]['last']
-    print(first, second, third)
+    first = avg_list[-1]['last']
+    second = avg_list[-2]['last']
+    third = avg_list[-3]['last']
     return round((first-second)/(second-third), 2)
 
-print(rate())
-
-# json_diff = [{'fields': {'value': diff()}, 'measurement': 'covid_confirmed_diff'}]
-# json_rate = [{'fields': {'value': rate()}, 'measurement': 'covid_confirmed_rate'}]
-# client.write_points(json_diff)
-# client.write_points(json_rate)
+json_diff = [{'fields': {'value': diff()}, 'measurement': 'covid_confirmed_diff'}]
+json_rate = [{'fields': {'value': rate()}, 'measurement': 'covid_confirmed_rate'}]
+client.write_points(json_diff)
+client.write_points(json_rate)
