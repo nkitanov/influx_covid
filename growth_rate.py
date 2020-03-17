5f036430105c27bfdaaa1f1b10d7a2b3ae2edad6
Nikolay Kitanov <nki@melexis.com>
2020-03-17 11:59:06 +0200

    add option to calculate daily cases and save in db

--- growth_rate.py
+++ growth_rate.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 from influxdb import InfluxDBClient
+from datetime import datetime, timedelta
 
 client = InfluxDBClient(host="192.168.1.201", port=8086, database="home_assistant")
 
+today = datetime.today().strftime("%Y-%m-%d")
+yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 
-def rate():
+
+def rate(what):
     daily_last = client.query(
         "select last(value) from people where entity_id='bulgaria_coronavirus_confirmed' group by time(1d)"
     )
@@ -16,17 +20,52 @@ def rate():
     first = daily_last_list[-1]["last"]
     second = daily_last_list[-2]["last"]
     third = daily_last_list[-3]["last"]
-    return round((first - second) / (second - third), 2)
+    if what == "rate":
+        return round((first - second) / (second - third), 2)
+    elif what == "today":
+        return first
 
 
 def db_rate():
-    ratedb = client.query(" select last(value) from covid_current_rate")
+    ratedb = client.query("select last(value) from covid_current_rate")
     ratedb_list = []
     for i in ratedb.get_points():
         ratedb_list.append(i)
     return ratedb_list[-1]["last"]
 
 
-if rate() != db_rate():
-    json_rate = [{"fields": {"value": rate()}, "measurement": "covid_current_rate"}]
+def yesterday_cases():
+    qyesterday = client.query(
+        "select last(value) from people where entity_id='bulgaria_coronavirus_confirmed' and time < '"
+        + today
+        + "' group by time(1d)"
+    )
+    qyesterday_list = []
+    for i in qyesterday.get_points():
+        qyesterday_list.append(i)
+    if len(qyesterday_list) == 0:
+        return 0
+    else:
+        return qyesterday_list[-1]["last"]
+
+
+def db_today():
+    qdb_today = client.query("select last(value) from covid_today_confirmed")
+    qdb_today_list = []
+    for i in qdb_today.get_points():
+        qdb_today_list.append(i)
+    return qdb_today_list[-1]["last"]
+
+
+if rate("rate") != db_rate():
+    json_rate = [
+        {"fields": {"value": rate("rate")}, "measurement": "covid_current_rate"}
+    ]
+    client.write_points(json_rate)
+
+day_cases = rate("today") - yesterday_cases()
+if rate("today") != day_cases:
+    json_rate = [
+        {"fields": {"value": day_cases}, "measurement": "covid_today_confirmed"}
+    ]
     client.write_points(json_rate)
