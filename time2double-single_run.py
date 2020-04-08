cba9e2d1630931f1202f6327e11083ea9da8a209
Your Name <nkitanov@gmail.com>
2020-04-08 15:29:58 +0300

    use epoch for time2double calculations

--- time2double-single_run.py
+++ time2double-single_run.py
@@ -5,31 +5,24 @@ from datetime import date
 client = InfluxDBClient(host="192.168.1.201", port=8086, database="home_assistant")
 
 # Build list with all days
-q = client.query("select last(value) from people where entity_id='bulgaria_coronavirus_confirmed' and time > '2020-03-11T00:00:00Z'  group by time(1d)")
+q = client.query("select last(value) from people where entity_id='bulgaria_coronavirus_confirmed' and time > '2020-03-11T00:00:00Z'  group by time(1d)", epoch='s')
 data = []
 for i in q.get_points():
     data.append(i)
 
 for x in data:
-    dateh = x["time"].split("T")[0].split("-")
-    yearh = int(dateh[0])
-    monthh = int(dateh[1])
-    dayh = int(dateh[2])
+    dateh = x["time"]
     half_value =str(x["last"] / 2)
     if float(half_value) <= 3.5: # skip it for the first days
-        continue
+         continue
 
-    q = client.query("select last(value) from people where entity_id='bulgaria_coronavirus_confirmed' and value < " + half_value + "" )
+    q = client.query("select last(value) from people where entity_id='bulgaria_coronavirus_confirmed' and value < " + half_value + "", epoch='s')
     list = []
     for i in q.get_points():
         list.append(i)
-    datel = list[0]["time"].split("T")[0].split("-")
-    yearl = int(datel[0])
-    monthl = int(datel[1])
-    dayl = int(datel[2])
+    datel = list[0]["time"]
     
-    diff = (date(yearh, monthh, dayh) - date(yearl, monthl, dayl)).days
-    date_db = (str(yearh) + "-" + str(monthh) + "-" + str(dayh))
+    days = round((dateh - datel) / 86400, 1) 
 
-    json = [{"fields": {"value": diff}, "measurement": "covid_timedouble", "time": date_db}]
-    client.write_points(json)
+    json = [{"fields": {"value": days}, "measurement": "covid_timedouble", "time": dateh}]
+    client.write_points(json, time_precision='s')
---
+++ time2double.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from influxdb import InfluxDBClient
+from datetime import date
+
+client = InfluxDBClient(host="192.168.1.201", port=8086, database="home_assistant")
+
+q = client.query(
+    "select last(value) from people where entity_id='bulgaria_coronavirus_confirmed'",
+    epoch="s",
+)
+
+for i in q.get_points():
+    timeh = i["time"]
+    half_value = str(i["last"] / 2)
+
+    q = client.query(
+        "select last(value) from people where entity_id='bulgaria_coronavirus_confirmed' and value < "
+        + half_value
+        + "",
+        epoch="s",
+    )
+    list = []
+    for i in q.get_points():
+        list.append(i)
+    timel = list[0]["time"]
+    days = round((timeh - timel) / 86400, 1)
+
+
+def db_time2double():
+    q = client.query("select last(value) from covid_timedouble")
+    list = []
+    for i in q.get_points():
+        list.append(i)
+    return list[0]["last"]
+
+
+if days != db_time2double():
+    json = [{"fields": {"value": days}, "measurement": "covid_timedouble"}]
+    client.write_points(json)
