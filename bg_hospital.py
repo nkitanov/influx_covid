084f412457cc15425e7c13ab3d5eccdcde8b7c8a
Nikolay Kitanov <nki@melexis.com>
2020-12-31 14:55:06 +0200

    add vaccinated and remove unnededded calulation of rates (done by influx query)

--- bg_hospital.py
+++ bg_hospital.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 from influxdb import InfluxDBClient
 from datetime import datetime
+from influx_connection import client
 import requests
 import sys
 
@@ -11,13 +12,6 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 utc_hour = int(datetime.utcnow().strftime("%H"))
 
-# Influx host is different if I run it from my Win PC
-if sys.platform == "linux":
-    influx_host = "localhost"
-else:
-    influx_host = "35.207.86.81"
-
-client = InfluxDBClient(host=influx_host, port=8086, database="covid_global")
 stats = soup.find(
     "div", attrs={"class": "row statistics-container inner-page-statistics"}
 )
@@ -28,6 +22,8 @@ def pull_stats(what):
         return int(stats.find_all("p")[12].text)
     elif what == "in_icu":
         return int(stats.find_all("p")[14].text)
+    elif what == "vaccinated":
+        return int(stats.find_all("p")[20].text)
 
 
 def db_stats(what):
@@ -40,18 +36,15 @@ def db_stats(what):
         return int(lst[0]["last_in_icu"])
     elif what == "active":
         return int(lst[0]["last_active"])
+    elif what == "vaccinated":
+        return int(lst[0]["last_vaccinated"])
 
 
-def rate(what):
-    if what == "hospitalization":
-        return float(round(pull_stats("hospitalized") / db_stats("active"), 2))
-    if what == "icu":
-        return float(round(pull_stats("in_icu") / db_stats("hospitalized"), 2))
-
-
-if pull_stats("hospitalized") != db_stats("hospitalized") or pull_stats(
-    "in_icu"
-) != db_stats("in_icu"):
+if (
+    pull_stats("hospitalized") != db_stats("hospitalized")
+    or pull_stats("in_icu") != db_stats("in_icu")
+    or pull_stats("vaccinated") != db_stats("vaccinated")
+):
     json_data = [
         {
             "measurement": "data",
@@ -59,19 +52,10 @@ if pull_stats("hospitalized") != db_stats("hospitalized") or pull_stats(
             "fields": {
                 "hospitalized": pull_stats("hospitalized"),
                 "in_icu": pull_stats("in_icu"),
+                "vaccinated": rate("vaccinated"),
             },
         }
     ]
-    json_rates = [
-        {
-            "measurement": "rates",
-            "tags": {"region": "Bulgaria"},
-            "fields": {
-                "hospitalization_rate": rate("hospitalization"),
-                "icu_rate": rate("icu"),
-            },
-        }
-    ]
+
     if utc_hour < 21:
         client.write_points(json_data)
-        client.write_points(json_rates)
