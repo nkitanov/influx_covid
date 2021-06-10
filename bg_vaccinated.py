57db1f2c444b31f6fb00fa2622959ece8fcd67da
Nikolay Kitanov <nkitanov@gmail.com>
2021-06-10 18:09:44 +0300

    reuse population data

--- bg_vaccinated.py
+++ bg_vaccinated.py
@@ -5,102 +5,42 @@ from bs4 import BeautifulSoup
 from datetime import date, datetime
 import requests
 import yaml
+from get_bg_regions import population_data
 
-# Население към 31.12.2019
-population_yaml = """
-    Благоевград: 
-        population: 302694
-        name: Blagoevgrad
-    Бургас:
-        population: 409265
-        name: Burgas
-    Варна: 
-        population: 469885
-        name: Varna
-    Велико Търново:
-        population: 232568
-        name: Veliko Tarnovo
-    Видин:
-        population: 82835
-        name: Vidin
-    Враца: 
-        population: 159470
-        name: Vratsa
-    Габрово:
-        population: 106598
-        name: Gabrovo
-    Добрич:
-        population: 171809
-        name: Dobrich
-    Кърджали:
-        population: 158204
-        name: Kardzhali
-    Кюстендил:
-        population: 116915
-        name: Kazanlak
-    Ловеч:
-        population: 122546
-        name: Lovech
-    Монтана:
-        population: 127001
-        name: Montana
-    Пазарджик:
-        population: 252776
-        name: Pazardzhik
-    Перник:
-        population: 119190
-        name: Pernik
-    Плевен:
-        population: 236305
-        name: Pleven
-    Пловдив:
-        population: 666801
-        name: Plovdiv
-    Разград:
-        population: 110789
-        name: Razgrad
-    Русе:
-        population: 215477
-        name: Ruse
-    Силистра:
-        population: 108018
-        name: Silistra
-    Сливен:
-        population: 184119
-        name: Sliven
-    Смолян:
-        population: 103532
-        name: Smolyan
-    София:
-        population: 226671
-        name: Sofia-obl
-    София (столица):
-        population: 1328790
-        name: Sofia-town
-    Стара Загора:
-        population: 313396
-        name: Stara Zagora
-    Търговище:
-        population: 110914
-        name: Targovishte
-    Хасково:
-        population: 225317
-        name: Haskovo
-    Шумен:
-        population: 172262
-        name: Shumen
-    Ямбол:
-        population: 117335
-        name: Yambol
-    Общо:
-        population: 6951482
-        name: Total
+town_map = """
+BLG_ALL: Благоевград
+BGS_ALL: Бургас
+VAR_ALL: Варна
+VTR_ALL: Велико Търново
+VID_ALL: Видин
+VRC_ALL: Враца
+GAB_ALL: Габрово
+DOB_ALL: Добрич
+KRZ_ALL: Кърджали
+KNL_ALL: Кюстендил
+LOV_ALL: Ловеч
+MON_ALL: Монтана
+PAZ_ALL: Пазарджик
+PER_ALL: Перник
+PVN_ALL: Плевен
+PDV_ALL: Пловдив
+RAZ_ALL: Разград
+RSE_ALL: Русе
+SLS_ALL: Силистра
+SLV_ALL: Сливен
+SML_ALL: Смолян
+SFO_ALL: София
+SOF_ALL: София (столица)
+SZR_ALL: Стара Загора
+TGV_ALL: Търговище
+HKV_ALL: Хасково
+SHU_ALL: Шумен
+JAM_ALL: Ямбол
 """
 
 URL = "https://coronavirus.bg/bg/statistika"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
-population_data = yaml.safe_load(population_yaml)
 time = date.today().strftime("%Y-%m-%d") + "T00:00:00Z"
 utc_hour = int(datetime.utcnow().strftime("%H"))
 
@@ -111,8 +51,23 @@ raw_data = (
     .find_all("td")
 )
 
+# Use population data from get_bg_regions and map the names
+town_map = yaml.safe_load(town_map)
+population_dict = {}
+for region in population_data:
+    if region != "TOTAL":
+        population_dict[town_map[region]] = {
+            "population": population_data[region]["population"],
+            "name": population_data[region]["name"],
+        }
+    else:
+        population_dict["Общо"] = {
+            "population": population_data["TOTAL"]["population"],
+            "name": "Total",
+        }
+
 # List total vaccinated data as dict
-# {'Blagoevgrad': {'total': 842, 'comirnaty': 211, 'moderna': 0, 'total_doses': 177},
+# {'Blagoevgrad': {'total': 842, 'total_doses': 177},
 def data_total():
     index = 0
     d = {}
@@ -121,7 +76,7 @@ def data_total():
         total_doses = raw_data[index + 6].text
         if total == "-":
             total = 0
-        d[population_data[raw_data[index].text]["name"]] = {
+        d[population_dict[raw_data[index].text]["name"]] = {
             "total": int(total),
             "total_doses": int(total_doses),
         }
@@ -135,9 +90,9 @@ def data_percent():
     index = 0
     d = {}
     while index < len(raw_data):
-        d[population_data[raw_data[index].text]["name"]] = round(
+        d[population_dict[raw_data[index].text]["name"]] = round(
             int(raw_data[index + 6].text)
-            / population_data[raw_data[index].text]["population"],
+            / population_dict[raw_data[index].text]["population"],
             4,
         )
         index += 7
@@ -146,17 +101,17 @@ def data_percent():
 
 def data_second_dose():
     d = {}
-    for region in population_data:
-        en_name = population_data[region]["name"]
+    for region in population_dict:
+        en_name = population_dict[region]["name"]
         d[en_name] = data_total()[en_name]["total_doses"]
     return d
 
 
 # Generate simplified dict for import totals in influx as the data_total function provides more data
 data_all = {}
-for region in population_data:
-    data_all[population_data[region]["name"]] = data_total()[
-        population_data[region]["name"]
+for region in population_dict:
+    data_all[population_dict[region]["name"]] = data_total()[
+        population_dict[region]["name"]
     ]["total"]
 
 json_total = [
