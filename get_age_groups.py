bd0922a4c80d6e20a7e46c1375c7c6b918118996
Nikolay Kitanov <nkitanov@gmail.com>
2021-10-03 14:43:56 +0300

    fix data when more age groups added

--- get_age_groups.py
+++ get_age_groups.py
@@ -7,6 +7,11 @@ from opendata import Opendata
 # Until 31.12.2020 per 100k
 population_data = {
     "total": 6916548 / 100000,
+    "0-1": 59029 / 100000,
+    "1-5": 255341 / 100000,
+    "6-9": 337290 / 100000,
+    "10-14": 347354 / 100000,	
+    "15-19": 314238 / 100000,
     "0-19": 1313252 / 100000,
     "20-29": 670806 / 100000,
     "30-39": 953393 / 100000,
@@ -23,9 +28,10 @@ opendata = Opendata("8f62cfcf-a979-46d4-8317-4e1ab9cbd6a8")
 data = opendata.data()
 
 # Generate json body with influx data for the last 1w only
-for day in data[-7:]:
-    time = day[0].replace("/", "-") + "T00:00:00Z"  # Time in influx format
+for record in data[-14:]:
     total = 0
+    day = [ '0' if x == '-' else x for x in record ]
+    time = day[0].replace("/", "-") + "T00:00:00Z"  # Time in influx format
     
     for num in day[1:]:
         total += int(num)
@@ -35,15 +41,20 @@ for day in data[-7:]:
             "measurement": "bg_age_groups",
             "time": time,
             "fields": {
-                "0-19":  round(int(day[1]) / population_data['0-19']),
-                "20-29": round(int(day[2]) / population_data['20-29']),
-                "30-39": round(int(day[3]) / population_data['30-39']),
-                "40-49": round(int(day[4]) / population_data['40-49']),
-                "50-59": round(int(day[5]) / population_data['50-59']),
-                "60-69": round(int(day[6]) / population_data['60-69']),
-                "70-79": round(int(day[7]) / population_data['70-79']),
-                "80-89": round(int(day[8]) / population_data['80-89']),
-                "90+":   round(int(day[9]) / population_data['90+']),
+                "0-1": round(int(day[1]) / population_data['0-1']),
+                "1-5": round(int(day[2]) / population_data['1-5']),
+                "6-9": round(int(day[3]) / population_data['6-9']),
+                "10-14": round(int(day[4]) / population_data['10-14']), 	
+                "15-19": round(int(day[5]) / population_data['15-19']),
+                "0-19":  round(int(day[6]) / population_data['0-19']),
+                "20-29": round(int(day[7]) / population_data['20-29']),
+                "30-39": round(int(day[8]) / population_data['30-39']),
+                "40-49": round(int(day[9]) / population_data['40-49']),
+                "50-59": round(int(day[10]) / population_data['50-59']),
+                "60-69": round(int(day[11]) / population_data['60-69']),
+                "70-79": round(int(day[12]) / population_data['70-79']),
+                "80-89": round(int(day[13]) / population_data['80-89']),
+                "90+":   round(int(day[14]) / population_data['90+']),
                 "total": round(total / population_data['total'])
             },
         }
